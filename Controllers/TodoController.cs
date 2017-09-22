7ea479174d9be39ef9f5ddf8a5b4a40e6ee62f63
digitopoi <hubo85@gmail.com>
2017-09-22 15:28:32 -0500

    Inject todo item service into todo controller

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -2,12 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreTodo.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreTodo.Controllers 
 {
     public class TodoController : Controller
     {
+        private readonly ITodoItemService _todoItemService;
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
         public IActionResult Index()
         {
             //  Get to-do items from database
